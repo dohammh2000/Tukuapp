b17d27d28e213a169e665a3acf8a1b6be338a765
Doha ali <133542985+dohammh2000@users.noreply.github.com>
2026-09-08 02:03:42 +0300

    Add files via upload

---
+++ lib/components/list_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:tuku/components/item_info.dart';
+import 'package:tuku/models/item.dart';
+
+class ListItem extends StatelessWidget {
+  const ListItem({super.key, required this.item, required this.color});
+  final ItemModel item;
+
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: color,
+      child: Row(
+        children: [
+          Container(
+            color: const Color(0xffFFF6DC),
+            child: Image.asset(item.image!),
+          ),
+          Expanded(child: ItemInfo(item: item)),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/phrase_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tuku/components/item_info.dart';
+import 'package:tuku/models/item.dart';
+
+class PhrasesItem extends StatelessWidget {
+  const PhrasesItem({super.key, required this.item, required this.color});
+
+  final ItemModel item;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: color,
+      child: ItemInfo(item: item),
+    );
+  }
+}
